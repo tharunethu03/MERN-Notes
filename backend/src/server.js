787ff870861db870bb55db9faa7849d938ef9c00
tharunethu03 <tharunethu03@icloud.com>
2025-09-22 01:01:02 +0530

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,28 +4,40 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-// middleware
-app.use(cors());
-app.use(express.json()); // this middleware will parse the JSON bodies: req.body
+// Middleware
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
+
+app.use(express.json());
 app.use(rateLimiter);
-
-
-// Example middleware (only console.log the message)
-// app.use((req, res, next) => {
-//   console.log(`request method: ${req.method}, request url is: ${req.url}`);
-//   next() // without this the request won't happen
-// });
 
 app.use("/api/notes", notesRoutes);
 
+// Serve frontend in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+  });
+}
+
+// Connect DB and start server
 connectDB().then(() => {
-  app.listen(5001, () => {
-    console.log("Server started on PORT:", PORT);
+  app.listen(PORT, () => {
+    console.log(`Server started on PORT: ${PORT}`);
   });
 });
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({
-    baseURL : "http://localhost:5001/api"
-})
+// in production, there's no localhost so we have to make this dynamic
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
 
-export default axiosInstance
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:5001/api",
+});
+
+export default axiosInstance;
